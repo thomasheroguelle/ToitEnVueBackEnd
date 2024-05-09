3e260a9dbbfb9e50c9f77135bc95584d3ac17688
Thomas HEROGUELLE <totosh93270@gmail.com>
2024-05-09 17:27:08 +0200

    first commit

---
+++ src/main/java/com/masterpiece/ToitEnVueBackEnd/ToitEnVueBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.masterpiece.ToitEnVueBackEnd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToitEnVueBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToitEnVueBackEndApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/masterpiece/ToitEnVueBackEnd/ToitEnVueBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.masterpiece.ToitEnVueBackEnd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToitEnVueBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

0662c210cf9095200d7ba8ede34a19946e6e4923
Thomas HEROGUELLE <totosh93270@gmail.com>
2024-05-12 15:46:33 +0200

    feat/deleteBooking : works

--- src/main/java/com/masterpiece/ToitEnVueBackEnd/controller/housing/HousingController.java
+++ src/main/java/com/masterpiece/ToitEnVueBackEnd/controller/housing/HousingController.java
@@ -2,11 +2,14 @@ package com.masterpiece.ToitEnVueBackEnd.controller.housing;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.masterpiece.ToitEnVueBackEnd.dto.booking.BookingDetailsDto;
 import com.masterpiece.ToitEnVueBackEnd.dto.housing.CreateHousingDto;
 import com.masterpiece.ToitEnVueBackEnd.dto.housing.HousingDto;
 import com.masterpiece.ToitEnVueBackEnd.dto.housing.HousingFromUser;
 import com.masterpiece.ToitEnVueBackEnd.exceptions.file.EmptyFileException;
+import com.masterpiece.ToitEnVueBackEnd.repository.housing.HousingRepository;
 import com.masterpiece.ToitEnVueBackEnd.security.jwt.UserDetailsUtils;
+import com.masterpiece.ToitEnVueBackEnd.service.booking.BookingService;
 import com.masterpiece.ToitEnVueBackEnd.service.housing.HousingService;
 import io.jsonwebtoken.JwtException;
 import jakarta.validation.Valid;
@@ -14,6 +17,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -32,6 +36,10 @@ public class HousingController {
     private HousingService housingService;
     @Autowired
     private ObjectMapper objectMapper;
+    @Autowired
+    private BookingService bookingService;
+    @Autowired
+    private HousingRepository housingRepository;
 
     @PostMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
     public ResponseEntity<?> createHousing(@Valid @RequestPart("housing") String createHousingJson,
@@ -83,6 +91,28 @@ public class HousingController {
         }
         List<HousingFromUser> housingList = housingService.findHousingByUserId(userId);
         return ResponseEntity.ok(housingList);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> deleteHousingFromUser(@PathVariable Long id) {
+        HousingDto housingDto = housingService.getHousing(id);
+        if (housingDto == null) {
+            return ResponseEntity.notFound().build();
+        }
+
+        Long authenticatedUserId = UserDetailsUtils.getUserDetails().getId();
+        if (!Objects.equals(authenticatedUserId, housingDto.getUser_id())) {
+            return ResponseEntity.status(HttpStatus.FORBIDDEN).body("Vous n'êtes pas autorisé à supprimer cette annonce.");
+        }
+
+        List<BookingDetailsDto> bookingDetails = bookingService.getBookingDetailsByHousingId(id);
+        if (!bookingDetails.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Impossible de supprimer cette annonce car elle a des réservations associées.");
+        }
+
+        housingRepository.deleteById(id);
+
+        return ResponseEntity.ok().build();
     }
 
 
